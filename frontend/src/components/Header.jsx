d2cea2bd80081a6cf81521641fb8928ae5036d9e
James Farr <james.farr@jdplc.com>
2023-11-28 17:12:07 +0000

    Header component created

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Nav, Navbar, Container } from 'react-bootstrap/Nav';
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+
+        </Navbar>
+    </div>
+  )
+}
+
+export default Header
