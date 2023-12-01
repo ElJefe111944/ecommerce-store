2c468f3f7ff8834db16a028c5f8843523b9fbcb5
James Farr <james.farr@jdplc.com>
2023-12-01 11:20:04 +0000

    Custom boostrap styles added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { Nav, Navbar, Container } from 'react-bootstrap';
+import { FaWolfPackBattalion } from "react-icons/fa6";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="light" variant="light" expand="md" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Zapatero</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <FaWolfPackBattalion />&nbsp;Zapatero
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
