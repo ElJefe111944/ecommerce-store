67bbe9356b64631aa3cb929a483513946548ad0b
James Farr <james.farr@jdplc.com>
2023-12-01 10:16:28 +0000

    Bootstrap Navbar added to header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,11 +3,20 @@ import { FaShoppingCart, FaUser } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
-        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-
-        </Navbar>
-    </div>
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Zapatero</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/login">Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
