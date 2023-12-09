cc139233e42b349f132fc46e1c79cd09092d5b32
James Farr <james.farr@jdplc.com>
2023-12-09 11:43:32 +0000

    Conditional check added to seeder file

--- backend/seeder.js
+++ backend/seeder.js
@@ -6,9 +6,12 @@ import products from "./data/products.js";
 import User from "./models/userModel.js";
 import Product from "./models/productModel.js";
 import Order from "./models/orderModel.js";
-import connectDB from "./config/db";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB();
+
 // import data into a MongoDB database
 const importData = async () => {
     try {
@@ -29,8 +32,30 @@ const importData = async () => {
         // Logs a message indicating that the data import process has been completed.
         console.log('Data Imported'.green.inverse);
         process.exit();
-    } catch (error){
+    } catch (error) {
         console.log(`${error}`.red.inverse);
         process.exit(1);
     }
 };
+
+const destroyData = async () => {
+    try {
+        // Deletes all existing orders, users, and products in the database.
+        await Order.deleteMany();
+        await User.deleteMany();
+        await Product.deleteMany();
+
+        // Logs a message indicating that the data destroy process has been completed.
+        console.log('Data Destrpyed'.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(`${error}`.red.inverse);
+        process.exit(1);
+    }
+};
+
+if(process.argv[2] === '-destroy'){
+    destroyData();
+} else {
+    importData();
+}
