8f63aaa43cb9a47bbe01d839a5f7b9b564d6896e
James Farr <james.farr@jdplc.com>
2023-12-09 17:32:00 +0000

    productRoutes created

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+const router = express.Router();
+import products from './data/products.js';
+
+// All Products API
+router.get('/', (req,res) => {
+    res.json(products);
+});
+
+// Single Products API
+router.get('/:id', (req,res) => {
+    const product = products.find((product) => product._id === req.params.id);
+    res.json(product);
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -15,15 +15,6 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-// All Products API
-app.get('/api/products', (req,res) => {
-    res.json(products);
-});
-
-// Single Products API
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find((product) => product._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes)
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
