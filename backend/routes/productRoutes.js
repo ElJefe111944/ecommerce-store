82daef262942672c612c4a83767d20bd11a6d2b0
James Farr <james.farr@jdplc.com>
2023-12-09 18:12:07 +0000

    Get products from database

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,24 @@
 import express from "express";
 const router = express.Router();
-import products from '../data/products.js';
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
 
 // All Products API
-router.get('/', (req,res) => {
+router.get('/', asyncHandler(async (req,res) => {
+    const products = await Product.find({});
     res.json(products);
-});
+}));
 
 // Single Products API
-router.get('/:id', (req,res) => {
-    const product = products.find((product) => product._id === req.params.id);
-    res.json(product);
-});
+router.get('/:id', asyncHandler(async (req,res) => {
+    const product =  await Product.findById(req.params.id);
+
+    if(product){
+        return res.json(product);
+    }
+
+    res.status(404).json({ message: 'Product not found' });
+
+}));
 
 export default router;
