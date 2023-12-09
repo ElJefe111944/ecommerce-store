b99eea19e54ba641ca4682e7dcdf508b22e7175b
James Farr <james.farr@jdplc.com>
2023-12-09 11:14:29 +0000

    Import data to mongoDB function added in seeder file

--- backend/seeder.js
+++ backend/seeder.js
@@ -9,3 +9,28 @@ import Order from "./models/orderModel.js";
 import connectDB from "./config/db";
 
 dotenv.config();
+// import data into a MongoDB database
+const importData = async () => {
+    try {
+        // Deletes all existing orders, users, and products in the database.
+        await Order.deleteMany();
+        await User.deleteMany();
+        await Product.deleteMany();
+        // Inserts user data from the users array into the User model.
+        const createdUsers = await User.insertMany(users);
+        // Retrieves the ID of the first user (adminUser).
+        const adminUser = createdUsers[0]._id;
+        // Maps over the products array and adds the adminUser's ID to each product.
+        const samepleProducts = products.map((product) => {
+            return { ...product, user: adminUser }
+        });
+        // Inserts the modified product data into the Product model.
+        await Product.insertMany(samepleProducts);
+        // Logs a message indicating that the data import process has been completed.
+        console.log('Data Imported'.green.inverse);
+        process.exit();
+    } catch (error){
+        console.log(`${error}`.red.inverse);
+        process.exit(1);
+    }
+};
