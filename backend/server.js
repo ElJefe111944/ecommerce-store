62c4ea9ab66963403ec115dba927b7bb757b035b
James Farr <james.farr@jdplc.com>
2023-12-12 17:39:48 +0000

    Custom error handling middleware added

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 5000;
-
+// Database Connection:
 connectDB();
 
 const app = express();
 
+//Routes
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
-
 app.use('/api/products', productRoutes);
 
+// Error Handling Middleware:
+app.use(notFound);
+app.use(errorHandler);
+// Server Listening:
 app.listen(port, () => console.log(`Server running on port ${port}`));
