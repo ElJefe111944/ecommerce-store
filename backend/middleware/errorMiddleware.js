032edef8618626614d2b7ed8aa7deaf81ee8bbac
James Farr <james.farr@jdplc.com>
2023-12-12 17:19:18 +0000

    error handler added to errorMiddleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,31 @@
+// This middleware is designed to handle 404 errors, indicating that the requested resource is not found.
 const notFound = (req, res, next) => {
-    const error = new Error(`Now Found - ${req.originalUrl}`);
+    // It creates a new Error object with a message containing the original URL of the request.
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    // Sets the HTTP status code to 404 and passes the error to the next middleware in the chain.
     res.status(404);
     next(error);
 };
+// This middleware is meant to handle general errors that may occur during the processing of a request.
+const errorHandler = (err, req, res, next) => {
+    // It checks the HTTP status code set in the response. If it's 200 (OK), it assumes an internal server error and sets the status code to 500.
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    // It extracts the error message and stack trace from the error object.
+    let message = err.message;
+
+
+    // If the error is a mongoose CastError and has the kind 'ObjectId', 
+    // it is treated as a resource not found error. In this case, 
+    // it sets the status code to 404 and changes the error message.
+    if(err.name === 'CastError' && err.kind === 'ObjectId'){
+        message = `Resource not found`;
+        statusCode = 404;
+    }; 
+    //Finally, it sends a JSON response with the appropriate status code, error message, and, in production mode, a simplified stack trace.
+    res.status(statusCode).json({
+        message,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+    });
+};
+
+export { notFound, errorHandler };
