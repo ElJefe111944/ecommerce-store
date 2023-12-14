7ae084ccafc41499e2eaa66a5b48a8b43d6e852e
James Farr <james.farr@jdplc.com>
2023-12-14 16:07:20 +0000

    productsApiSlice created

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,17 @@
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlices";
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: () => ({
+                url: PRODUCTS_URL,
+            }),
+            keepUnusedDataFor: 5
+        })
+    }),
+});
+
+// Export hooks for usage in function components, which are
+// auto-generated based on the defined endpoints
+export const { useGetProductsQuery } = productsApiSlice;
