4655be9112307a33220c01add4f1257cbf1ee99b
James Farr <james.farr@jdplc.com>
2023-12-16 12:18:28 +0000

    Items in cart counter added to header link from state

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Badge, Nav, Navbar, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap'
 import { FaWolfPackBattalion } from "react-icons/fa6";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg="white" data-bs-theme="white" variant="white" expand="md" collapseOnSelect>
@@ -16,8 +20,16 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link><FaShoppingCart />&nbsp; Cart</Nav.Link>
+              <LinkContainer style={{
+                    display: 'flex',
+                    alignItems: 'center',
+              }} to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />&nbsp; Cart
+                  {cartItems.length > 0 && (
+                    <Badge style={{ marginLeft: '5px' }} pill bg='black'>{cartItems.reduce((acc, current) => acc + current.qty, 0)}</Badge>
+                  )}
+                </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
                 <Nav.Link><FaUser />&nbsp; Sign In</Nav.Link>
