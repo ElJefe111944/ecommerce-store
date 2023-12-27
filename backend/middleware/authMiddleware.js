b0fbd452a94e9b6a770c6825ba0d2e5b77ed7842
James Farr <james.farr@jdplc.com>
2023-12-27 10:34:17 +0000

    Admin middleware added to authMiddleware file

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import asyncHandler from "./asyncHandler.js";
 import User from "../models/userModel.js";
 
 // Protect routes
-export const protect = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
     let token;
 
     // Read JWT from cookie
@@ -25,3 +25,16 @@ export const protect = asyncHandler(async (req, res, next) => {
         throw new Error('Not authorised, no token');
     };
 }); 
+
+// Admin middleware
+const admin = (req, res, next) => {
+    if(req.user && req.user.isAdmin){
+        next();
+
+    } else {
+        res.status(401);
+        throw new Error('Not authorised as admin');
+    };
+};
+
+export { admin, protect };
