408377ebef9963d9ed14260a542a774cd506e21d
James Farr <james.farr@jdplc.com>
2023-12-28 10:13:28 +0000

    Get user profile controller added to user controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,7 +14,7 @@ const authUser = asyncHandler(async (req, res) => {
 
         generateToken(res, user._id);
       
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -78,7 +78,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 // route: GET /api/users/profile
 // access: private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('Get User profile');
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    };
 });
 
 // description: update user profile
