58edb3938598140ae234563a24b1780e4b743209
James Farr <james.farr@jdplc.com>
2023-12-30 12:56:29 +0000

    Paymentscreen created and routing added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PrivateRoute from './components/PrivateRoute';
+import PaymentScreen from './screens/PaymentScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,9 +25,10 @@ const router = createBrowserRouter(
         <Route path='/cart' element={<CartScreen />} />
         <Route path='/login' element={<LoginScreen />} />
         <Route path='/register' element={<RegisterScreen />} />
-        {/* Private Routes */}
+        {/* Private/Protected Routes */}
         <Route path='' element={<PrivateRoute />} >
           <Route path='/shipping' element={<ShippingScreen />} />
+          <Route path='/payment' element={<PaymentScreen />} />
         </Route>
       </Route>
   )
