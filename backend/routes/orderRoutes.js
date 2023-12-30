98539c5e0d07bf3c4782c32360043f829cf792da
James Farr <james.farr@jdplc.com>
2023-12-30 13:34:19 +0000

    addOrderItems,     getAllOrders,     getMyOrders,     getOrderById,     updateToDelivered,     updateOrderToPaid,     updateToDelivered routes added to order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+const router = express.Router();
+import { 
+    addOrderItems,
+    getAllOrders,
+    getMyOrders,
+    getOrderById,
+    updateToDelivered,
+    updateOrderToPaid,
+    updateToDelivered
+ } from "../controllers/orderController.js";
+
+ import { protect, admin } from "../middleware/authMiddleware.js"
+
+ // All folowing routes start with /api/orders/...
+
+// GET - get all orders (admin) or POST - add order items (registered/admin)
+router.route('/').get(protect, admin, getAllOrders).post(protect, addOrderItems); 
+// GET - get my orders - registered users
+router.route('/mine').get(protect, getMyOrders);
+// GET - order by ID - Admin users 
+router.route('/:id').get(protect, admin, getOrderById);
+// PUT - update to paid - admin/registered
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+// PUT - update to delivered - admin/registered
+router.route('/:id/deliver').put(protect, updateToDelivered);
+
+
+export default router;
