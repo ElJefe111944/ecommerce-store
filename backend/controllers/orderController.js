1d8ed7d56bdb5d6989b8e0e48dd03f584a5dfa4e
James Farr <james.farr@jdplc.com>
2023-12-31 14:17:53 +0000

    Update order to paid added to order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -73,11 +73,29 @@ const getOrderById = asyncHandler(async (req, res) => {
 // access: private
 
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-    res.send('Update order to paid');
+    const order = await Order.findById(req.params.id);
+
+    if (order){
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+        };
+
+        const updateOrder = await order.save();
+
+        res.status(200).json(updateOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 });
 
 // description: Update order to delievered
-// route: GET /api/orders/:id/deliver
+// route: PUT /api/orders/:id/deliver
 // access: private/admin
 
 const updateToDelivered = asyncHandler(async (req, res) => {
