38e7ee31500c9f89309dd2e54fcc70997e4d69af
James Farr <james.farr@jdplc.com>
2024-01-03 14:44:21 +0000

    create product added as POST request in product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductById } from "../controllers/productController.js";
+import { getProducts, getProductById, createProduct } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 // All Products API
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 // Single Products API
 router.route('/:id').get(getProductById);
 
