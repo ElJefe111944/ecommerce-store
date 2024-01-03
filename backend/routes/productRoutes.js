52ee624de29774b0a9c729ae399e42a6275be753
James Farr <james.farr@jdplc.com>
2024-01-03 16:58:51 +0000

    updated product added to product routes as PUT request

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductById, createProduct } from "../controllers/productController.js";
+import { getProducts, getProductById, createProduct, updateProducts } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 // All Products API
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 // Single Products API
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).put(protect, admin, updateProducts);
 
 export default router;
