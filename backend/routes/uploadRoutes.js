cd15992da8d96794f59fdcd8341f18e85c0a7062
James Farr <james.farr@jdplc.com>
2024-01-06 13:48:20 +0000

    Image upload endpoint created in uploadRoutes

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,41 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    }
+})
+
+function fileFilter(req, file, cb) {
+    const filetypes = /jpe?g|png|webp/;
+    const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = mimetypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        cb(null, true);
+    } else {
+        cb(new Error('Images only!'), false);
+    }
+
+}
+
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single('image');
+
+router.post('/', upload.single('image'),  (req, res) => {
+    res.send({
+        message: 'Image Uploaded',
+        image: `/${req.file.path}`,
+    });
+  });
+
+  export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -6,7 +7,8 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js"
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 const port = process.env.PORT || 5000;
 // Database Connection:
@@ -27,11 +29,16 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-
+app.use('/api/upload', uploadRoutes);
 // Paypal 
 app.get('/api/config/paypal', (req, res) => res.send({ 
     clientId: process.env.PAYPAL_CLIENT_ID
  }));
+
+// set __dirname to current directory
+const __dirname = path.resolve(); 
+// set upload folder as static
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Error Handling Middleware:
 app.use(notFound);
