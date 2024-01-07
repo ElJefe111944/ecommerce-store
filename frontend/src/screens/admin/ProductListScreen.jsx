5c4db3de911a26fe6a5e5c3be08f4b5234b90b23
James Farr <james.farr@jdplc.com>
2024-01-07 13:18:52 +0000

    Delete product functionality added to product list screen and tested

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -4,7 +4,7 @@ import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa';
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import { toast } from "react-toastify";
-import { useGetProductsQuery, useCreateProductMutation } from '../../slices/productsApiSlice';
+import { useGetProductsQuery, useCreateProductMutation, useDeleteProductMutation } from '../../slices/productsApiSlice';
 
 const ProductListScreen = () => {
 
@@ -12,75 +12,87 @@ const ProductListScreen = () => {
 
     const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
 
-    const deleteHandler = (productId) => {
-        console.log(`Product with ID: ${productId} clicked`);
-    };
+    const [deleteProduct, { isLoading: loadingDelete }] = useDeleteProductMutation();
 
-    const createProductHandler = async () => {
-        if(window.confirm('Are you sure you want to create a new product?')){
+    const deleteHandler = async (productId) => {
+        if (window.confirm('Are you sure you want to Delete this product?')) {
             try {
-                await createProduct();
+                await deleteProduct(productId);
                 refetch();
-            
-            } catch (error){
+                toast.success('Product successfully deleted');
+            } catch (error) {
                 toast.error(error?.data?.message || error?.error);
             };
-        }
-    };
+        };
+    }
+
+        const createProductHandler = async () => {
+            if (window.confirm('Are you sure you want to create a new product?')) {
+                try {
+                    await createProduct();
+                    refetch();
+                    toast.success('Product successfully created');
+                } catch (error) {
+                    toast.error(error?.data?.message || error?.error);
+                };
+            }
+        };
 
 
-    return (
-        <div>
-            <Row className='align-items-center'>
-                <Col>
-                    <h1>Products</h1>
-                </Col>
-                <Col className='text-end'>
-                    <Button className='btn-sm m-3' onClick={createProductHandler}>
-                        <FaEdit /> Create Product
-                    </Button>
-                </Col>
-            </Row>
-            {loadingCreate && <Loader />}
-            {isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-                <>
-                    <Table striped hover responsive className='table-sm'>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>NAME</th>
-                                <th>PRICE</th>
-                                <th>CATEGORY</th>
-                                <th>BRAND</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {products.map((product) => (
-                                <tr key={product._id}>
-                                    <td>{product._id}</td>
-                                    <td>{product.name}</td>
-                                    <td>{product.price}</td>
-                                    <td>{product.category}</td>
-                                    <td>{product.brand}</td>
-                                    <td>
-                                        <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                                            <Button className="btn-sm mx-2" variant="light">
-                                                <FaEdit />
-                                            </Button>
-                                        </LinkContainer>
-                                        <Button className="btn-sm" variant="danger" onClick={() => deleteHandler(product._id)}>
+        return (
+            <div>
+                <Row className='align-items-center'>
+                    <Col>
+                        <h1>Products</h1>
+                    </Col>
+                    <Col className='text-end'>
+                        <Button className='btn-sm m-3' onClick={createProductHandler}>
+                            <FaEdit /> Create Product
+                        </Button>
+                    </Col>
+                </Row>
+                {loadingCreate && <Loader />}
+                {loadingDelete && <Loader />}
+                {isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+                    <>
+                        <Table striped hover responsive className='table-sm'>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>NAME</th>
+                                    <th>PRICE</th>
+                                    <th>CATEGORY</th>
+                                    <th>BRAND</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {products.map((product) => (
+                                    <tr key={product._id}>
+                                        <td>{product._id}</td>
+                                        <td>{product.name}</td>
+                                        <td>{product.price}</td>
+                                        <td>{product.category}</td>
+                                        <td>{product.brand}</td>
+                                        <td>
+                                            <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                                                <Button className="btn-sm mx-2" variant="light">
+                                                    <FaEdit />
+                                                </Button>
+                                            </LinkContainer>
+                                            <Button className="btn-sm" variant="danger" onClick={() => deleteHandler(product._id)}>
                                                 <FaTrash style={{ color: 'white' }} />
                                             </Button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-                </>
-            )}
-        </div>
-    )
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    </>
+                )}
+            </div>
+        )
 }
+
 
 export default ProductListScreen
