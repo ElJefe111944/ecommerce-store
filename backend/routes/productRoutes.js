e0d374adf56dece797c98bb5c0dd28b7867ea19f
James Farr <james.farr@jdplc.com>
2024-01-07 19:46:39 +0000

    create review route added to product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductById, createProduct, updateProducts, deleteProduct } from "../controllers/productController.js";
+import {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProducts,
+    deleteProduct,
+    createProductReview
+} from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 // All Products API
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 // Single Products API
 router.route('/:id').get(getProductById).put(protect, admin, updateProducts).delete(protect, admin, deleteProduct);
-
+// reviews
+router.route('/:id/reviews').post(protect, createProductReview);
 export default router;
