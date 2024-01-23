93b94e94b1cbf660d7ece664b3a350ba2ad5e717
James Farr <james.farr@jdplc.com>
2024-01-23 07:20:23 +0000

    Get top products endpoint added to products controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -139,6 +139,14 @@ const createProductReview = asyncHandler(async (req, res) => {
     }
 });
 
+// description: Get top rated products
+// route: GET /api/products/top
+// access: public
+const getTopProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+    res.status(200).json(products);
+});
 
 
-export { getProducts, getProductById, createProduct, updateProducts, deleteProduct, createProductReview };
+
+export { getProducts, getProductById, createProduct, updateProducts, deleteProduct, createProductReview, getTopProducts };
